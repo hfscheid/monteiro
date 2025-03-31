69c76bc57962137de5a59c36822481c139b5bd9b
Henrique Fürst <hfscheid@gmail.com>
2025-03-31 19:55:33 -0300

    feat: setup function

--- src/data.rs
+++ src/data.rs
@@ -1,11 +1,25 @@
 use std::fs;
-use std::io;
+use std::io::{self, ErrorKind};
+use homedir::my_home;
+use crate::toml as buildcfg;
 
-fn setup(dirname: &str) -> io::Result<bool> {
-    return Ok(true)
+fn setup() -> io::Result<()> {
+    // überprüfe, ob Verzeichnis schon existiert
+    let home = my_home().unwrap();
+    let monteiro_dir = &home.unwrap().join(".monteiro");
+    match fs::exists(monteiro_dir) {
+        Ok(true) => Ok(()),
+        Ok(false) => fs::create_dir(monteiro_dir),
+        _ => Err(io::Error::new(ErrorKind::PermissionDenied, "Could not create .monteiro directory")),
+    }
 }
 
-fn add() {
+fn add(filename: &str) -> io::Result<()> {
+    if let Ok(_) = buildcfg::read_build_cfg(filename) {
+        Ok(())
+    } else {
+        Err(io::Error::new(ErrorKind::InvalidInput, "Could not parse config file"))
+    }
 }
 
 fn remove() {
